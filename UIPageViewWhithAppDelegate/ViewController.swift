56921167bee5cd2ff585d7d1fcd4ee4ce48b8faa
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-20 20:07:08 +0200

    Initial Commit

---
+++ UIPageViewWhithAppDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageViewWhithAppDelegate
+//
+//  Created by Артем on 3/20/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
